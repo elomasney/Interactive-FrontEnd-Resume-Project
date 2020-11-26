acbca3d0928cc15538a28191b3ef06ec2d325096
elomasney <eimearlomasney01@gmail.com>
2020-11-26 17:23:55 +0000

    Added loader gif and the fetchGitHubInformation function

---
+++ assets/js/github-information.js
@@ -0,0 +1,28 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/images/loader.gif" alt="loading..." />
+        </div>`);
+
+        $.when(
+            $.getJSON(`https://api.github.com/users/${username}`)
+        ).then(
+            function(response){
+                var userData = response;
+                $("#gh-user-data").html(userInformationHTML(userData));
+            }, function(error){
+                if(errorResponse.staus==404){
+                    $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                }else {
+                    console.log(errorResponse);
+                    $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                }
+            });
+}
